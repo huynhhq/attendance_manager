655bf0cdc0bad34c29ab9c9cf12d2615575d6937
huynh hoang <huynhhq96@gmail.com>
2020-04-06 08:11:02 +0700

    fix attendace

--- routes/attendances.js
+++ routes/attendances.js
@@ -4,14 +4,60 @@ var connection  = require('../lib/db');
 var LibLogger = require('../lib/log');
 var libLogger = new LibLogger().getInstance();
 
+function userAttendance(id, name, fingerId, exployerId, inTime, outTime){
+
+}
 router.get('/', function(req, res, next) {
-      
-    connection.query('SELECT * FROM Attendances ORDER BY id desc',function(err,rows)     {
+     
+    connection.query('SELECT * FROM Users ORDER BY id desc',function(err,rows)     {
         if(err){
             req.flash('error', err); 
-            res.render('attendances',{page_title:"Attendances - Node.js",data:''});   
-        }else{                             
-            res.render('attendances',{page_title:"Attendances - Node.js",data:rows});
+            return res.render('users',{page_title:"Users - Node.js",data:''});   
+        }else{  
+            var attendanceArr = []; 
+            for (let index = 0; index < rows.length; index++) {
+                const element = rows[index];
+                var userAttendance = {};
+                userAttendance['id'] = element.id;
+                userAttendance['name'] = element.name;
+                userAttendance['code'] = element.code;
+
+                connection.query('SELECT * FROM Attendances where ( user_id = ' + element.id + ' AND DATE(created_at) = curdate() ) order by created_at limit 1',function(errInTime,inTimeRows) {
+                    
+                    if (errInTime) {
+                        req.flash('error', errInTime); 
+                        return res.render('attendances',{page_title:"Attendances - Node.js",data:''});   
+                    }
+                                        
+                    if (inTimeRows.length <= 0) {
+                        userAttendance[ 'inTime' ] = "Đang cập nhật";
+                        userAttendance[ 'outTime' ] = "Đang cập nhật";
+                    }
+                    else{
+                        userAttendance[ 'inTime' ] = inTimeRows[0].created_at;                           
+                        connection.query('SELECT * FROM Attendances where ( user_id = ' + element.id + ' AND id != ' + inTimeRows[0].id + ' AND DATE(created_at) = curdate() ) order by created_at DESC limit 1',function(errOutTime,inOutRows) {
+                            if (errOutTime) {
+                                req.flash('error', errOutTime); 
+                                return res.render('attendances',{page_title:"Attendances - Node.js",data:''});   
+                            }
+
+                            if( inOutRows.length <= 0)
+                            {
+                                userAttendance[ 'outTime' ] = "Đang cập nhật";                                 
+                            } 
+                            else
+                            {
+                                userAttendance[ 'outTime' ] = inOutRows[0].created_at;
+                            }                            
+                        });
+                    }                                       
+                });
+
+                attendanceArr.push( userAttendance );
+            }
+
+            setTimeout(function(){ res.render('attendances',{page_title:"Users - Node.js",data:attendanceArr}); }, 500);
+        
         }                            
     });        
 });
@@ -31,6 +77,68 @@ router.get('/delete/(:id)', function(req, res, next) {
         }
     })
 })
+
+
+router.get('export/(:id)', function*( req, res, next )
+{
+    connection.query('SELECT * FROM Users WHERE id = ' + req.params.id,function(err,rows)     {
+        if(err){
+            req.flash('error', err); 
+            return res.render('users',{page_title:"Users - Node.js",data:''});   
+        }else{  
+            var attendanceArr = []; 
+            for (let index = 0; index < rows.length; index++) {
+                const element = rows[index];
+                var userAttendance = {};
+                userAttendance['id'] = element.id;
+                userAttendance['name'] = element.name;
+                userAttendance['code'] = element.code;
+
+                connection.query('SELECT * FROM Attendances where ( user_id = ' + element.id + ' AND DATE(created_at) = curdate() ) order by created_at limit 1',function(errInTime,inTimeRows) {
+                    
+                    if (errInTime) {
+                        req.flash('error', errInTime); 
+                        return res.render('attendances',{page_title:"Attendances - Node.js",data:''});   
+                    }
+                                        
+                    if (inTimeRows.length <= 0) {
+                        userAttendance[ 'inTime' ] = "Đang cập nhật";
+                        userAttendance[ 'outTime' ] = "Đang cập nhật";
+                    }
+                    else{
+                        userAttendance[ 'inTime' ] = inTimeRows[0].created_at;                           
+                        connection.query('SELECT * FROM Attendances where ( user_id = ' + element.id + ' AND id != ' + inTimeRows[0].id + ' AND DATE(created_at) = curdate() ) order by created_at DESC limit 1',function(errOutTime,inOutRows) {
+                            if (errOutTime) {
+                                req.flash('error', errOutTime); 
+                                return res.render('attendances',{page_title:"Attendances - Node.js",data:''});   
+                            }
+
+                            if( inOutRows.length <= 0)
+                            {
+                                userAttendance[ 'outTime' ] = "Đang cập nhật";                                 
+                            } 
+                            else
+                            {
+                                userAttendance[ 'outTime' ] = inOutRows[0].created_at;
+                            }                            
+                        });
+                    }                                       
+                });
+
+                attendanceArr.push( userAttendance );
+            }
+
+            setTimeout(function(){ 
+                var json2csv = require('json2csv');
+                var fields = ['User ID', 'User Name', 'Employer ID', 'In Time', 'Out Time'];
+                var fieldNames = ['User ID', 'User Name', 'Employer ID', 'In Time', 'Out Time'];
+                var data = json2csv({ data: docs, fields: fields, fieldNames: fieldNames });
+                res.render('attendances',{page_title:"Users - Node.js",data:attendanceArr}); 
+            }, 500);
+        
+        }                            
+    });
+});
  
  
 module.exports = router;
